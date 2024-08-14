bbc38133afdb62cc7b61c76c09174ba9002ea324
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-08-14 16:00:23 +0530

    Added the sample

---
+++ DataCleaning/Model/Model.cs
@@ -0,0 +1,9 @@
+﻿namespace DataCleaning_Preprocessing
+{
+    public class WebsiteTrafficData
+    {
+        public DateTime DateTime { get; set; }
+
+        public double Visitors { get; set; }
+    }
+}
